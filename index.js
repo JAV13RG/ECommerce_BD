f715d9113503c8a312a49c0ead30ebde04ebb1a6
Javier <javitoprogrammer@gmail.com>
2025-06-25 19:05:06 -0400

    security: CORS, helmet y express rate limit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose'); // Database
-const cors = require('cors');
 const errorHandler = require('./middleware/errorHandler');
+const cors = require('cors');
+const helmet = require('helmet'); // Security
+const rateLimit = require('express-rate-limit'); // Rate limiting
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,6 +11,13 @@ const PORT = process.env.PORT || 5000;
 // Middlewares
 app.use(cors());
 app.use(express.json());
+app.use(helmet());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per windowMs
+});
+app.use(limiter);
 
 const productRoutes = require('./routes/productRoutes');
 app.use('/api/products', productRoutes);
